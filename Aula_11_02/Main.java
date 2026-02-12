3681d4e22376a878cec6b4295f4ba7160ca59d9d
Isadora Santos Leal <isadorasantos@unipam.edu.br>
2026-02-12 10:45:23 +0000

    Aula 11/02

--- Aula_11_02/Main.java
+++ Aula_11_02/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         
         Gerente Gerente = new Gerente();
-        Gerente.setNome("Arthur");
+        Gerente.setNome("Isadora Santos");
         System.out.println(Gerente.getNome());
 
 
